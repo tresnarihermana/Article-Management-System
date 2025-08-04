5dc2b4448dc818cecd62e040da09d39df8b461d8
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-04 16:10:05 +0700

    initialize migration for article, categories, tags table and pivot atau apalah itu article_tag and article_category

---
+++ database/migrations/2025_08_04_131033_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt')->nullable();
+            $table->longText('body');
+            $table->string('featured_image')->nullable();
+            $table->enum('status', ['draft', 'published', 'pending'])->default('draft');
+            $table->boolean('is_pinned')->default(false);
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
