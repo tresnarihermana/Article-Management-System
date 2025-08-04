c63bd41f45f54c269cdc24f8bc6505fc2519d285
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-04 16:15:56 +0700

    add new article,tags, and category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,14 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Tag;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Article;
+use App\Models\Comment;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RolePermissionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -32,7 +37,7 @@ class DatabaseSeeder extends Seeder
             'email_verified_at' => now(),
         ]);
         $admin->assignRole('admin');
-        
+
         $operator = User::factory()->create([
             'name' => 'Operator',
             'username' => 'operator',
@@ -40,7 +45,7 @@ class DatabaseSeeder extends Seeder
             'email_verified_at' => now(),
         ]);
         $operator->assignRole('operator');
-        
+
         $user = User::factory()->create([
             'name' => 'First User',
             'username' => 'user',
@@ -49,8 +54,16 @@ class DatabaseSeeder extends Seeder
         ]);
         $user->assignRole('user');
 
+     $tags = \App\Models\Tag::factory(10)->create();
+    $categories = \App\Models\Category::factory(5)->create();
+       $articles = Article::factory(10)->create()->each(function ($article) use ($tags, $categories) {
+    $article->tags()->attach($tags->random(rand(1, 3))->pluck('id'));
+    $article->categories()->attach($categories->random(rand(1, 2))->pluck('id'));
+});
 
-     User::factory(100)->create()->each(function ($user) {
+
+
+        User::factory(100)->create()->each(function ($user) {
             $user->assignRole('user');
         });
     }
