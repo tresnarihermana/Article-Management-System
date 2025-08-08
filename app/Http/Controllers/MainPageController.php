ab19fc15472959b10b70e2ce74dc20fd21252d64
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-08 21:46:23 +0700

    feat: (Main Page) add show article feature to controller

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -16,9 +16,33 @@ class MainPageController extends Controller
             ->limit(3)
             ->latest()
             ->get();
-        return Inertia::render('Welcome',[
+        return Inertia::render('Welcome', [
             "articles" => $article,
-            
+
+        ]);
+    }
+    public function show($slug)
+    {
+        $article = Article::with('tags', 'user', 'categories')
+            ->where('slug', $slug)
+            ->firstOrFail();
+                $articledata =  [
+                    "title" => $article->title,
+                    "body" => $article->body,
+                    "excerpt" => $article->excerpt,
+                    "user" => $article->user,
+                    "tags" => $article->tags,
+                    "categories" => $article->categories,
+                    'created_at' => $article->created_at->format('Y-m-d H:i:s'),
+                    'updated_at' => $article->updated_at->format('Y-m-d H:i:s'),
+                    'published_at' => $article->published_at->format('Y-m-d H:i:s'),
+                    "status" => $article->status,
+                    "slug" => $article->slug,
+                    "cover" => $article->cover,
+                    "id" => $article->id,
+                ];
+        return Inertia::render("Main/Read", [
+            "article" => $articledata,
         ]);
     }
 }
