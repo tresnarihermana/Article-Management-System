3befc6ba06df174f586626da97fdff3ca3b3c39c
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-08 22:04:58 +0700

    feat: (Layout) add new MainPage layout wiht navbar included

---
+++ resources/js/layouts/Main/PageSimpleLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Head, Link, usePage } from '@inertiajs/vue3';
+import PageNav from '@/components/PageNav.vue';
+const { articles, tags, categories, users, title } =usePage().props;
+</script>
+
+<template>
+
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <header>
+        <PageNav />
+    </header>
+     <slot />
+</template>
---
+++ resources/js/layouts/PageLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PageLayout from '../layouts/Main/PageSimpleLayout.vue'
+
+defineProps<{
+    title?: string;
+    description?: string;
+}>();
+</script>
+
+<template>
+    <PageLayout>
+        <slot />
+    </PageLayout>
+</template>
