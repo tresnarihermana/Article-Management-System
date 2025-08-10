c29b098f6bf7952e715b4437e79ba5e299936382
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-10 22:48:20 +0700

    fix: (Role and Permissions) make super admin have access to entire website

--- resources/js/lib/can.ts
+++ resources/js/lib/can.ts
@@ -1,7 +1,12 @@
-import {usePage} from '@inertiajs/vue3';
-export function can(permission: string) : boolean{
+import { usePage } from '@inertiajs/vue3';
+export function can(permission: string): boolean {
     const page = usePage();
+    const roles: string[] = page.props.auth.roles ?? [];
     const permissions: string[] = page.props.auth.permissions ?? [];
+    if (roles.includes('Super Admin')) {
+        return true;
+    }
     return permissions.includes(permission);
+
 
 }
