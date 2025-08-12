829c8e5636b6d5b5c8cfbe58cf44372c0596b53e
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-12 22:31:57 +0700

    refactor and feat: (Main) improve formating and add category to upcoming articles page

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Article;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -13,11 +14,11 @@ class MainPageController extends Controller
     {
         $article = Article::with('tags', 'user', 'categories')
             ->where('status', 'published')
-            ->limit(3)
             ->latest()
             ->get();
         return Inertia::render('Welcome', [
             "articles" => $article,
+
 
         ]);
     }
@@ -26,34 +27,35 @@ class MainPageController extends Controller
         $article = Article::with('tags', 'user', 'categories')
             ->where('slug', $slug)
             ->firstOrFail();
-                $articledata =  [
-                    "title" => $article->title,
-                    "body" => $article->body,
-                    "excerpt" => $article->excerpt,
-                    "user" => $article->user,
-                    "tags" => $article->tags,
-                    "categories" => $article->categories,
-                    'created_at' => $article->created_at->format('Y-m-d H:i:s'),
-                    'updated_at' => $article->updated_at->format('Y-m-d H:i:s'),
-                    'published_at' => $article->published_at->format('Y-m-d H:i:s'),
-                    "status" => $article->status,
-                    "slug" => $article->slug,
-                    "cover" => $article->cover,
-                    "id" => $article->id,
-                ];
+        $articledata =  [
+            "title" => $article->title,
+            "body" => $article->body,
+            "excerpt" => $article->excerpt,
+            "user" => $article->user,
+            "tags" => $article->tags,
+            "categories" => $article->categories,
+            'created_at' => $article->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $article->updated_at->format('Y-m-d H:i:s'),
+            'published_at' => $article->published_at->format('Y-m-d H:i:s'),
+            "status" => $article->status,
+            "slug" => $article->slug,
+            "cover" => $article->cover,
+            "id" => $article->id,
+        ];
         return Inertia::render("Main/Read", [
             "article" => $articledata,
+
         ]);
     }
     public function articles()
     {
-       $article = Article::with('tags', 'user', 'categories')
+        $article = Article::with('tags', 'user', 'categories')
             ->where('status', 'published')
             ->latest()
             ->get();
         return Inertia::render('Main/Articles', [
             "articles" => $article,
-
+            "categories" => Category::all()
         ]);
     }
 }
