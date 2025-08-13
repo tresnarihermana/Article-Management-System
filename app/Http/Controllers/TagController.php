8d496de985b634f9ce2cf95e96d1f49d830521e0
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-13 21:56:42 +0700

    feat: (Tags) add fully functional (resource) TagController with CRUD feature

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,10 +2,90 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Tag;
+use Inertia\Inertia;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TagController extends Controller
 {
-    //
+    public function index(Request $request)
+    {   
+        
+        $perPage = $request->input('perPage', 10);
+        $search = $request->input('search');
+        $tags = Tag::with('articles')
+            ->when($search, function ($query, $search) {
+                $query->where(function ($q) use ($search) {
+                    $q->where('name', 'like', "%$search%")
+                        ->orWhere('slug', 'like', "%$search%");
+                });
+            })
+            ->orderBy('id', 'desc')
+            ->paginate($perPage)
+            ->withQueryString()
+            ->through(function ($tag) {
+                return [
+                    'id' => $tag->id,
+                    'name' => $tag->name,
+                    'created_at' => $tag->created_at->format('Y-m-d H:i:s'),
+                    'updated_at' => $tag->updated_at->format('Y-m-d H:i:s'),
+                    'slug' => $tag->slug,
+                    'articles' => $tag
+                ];
+            });
+        return Inertia::render('Tags/Index', [
+            'tags' => $tags,
+            'search' => $search,
+            'filters' => $request->only('input')
+        ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Tags/Create', []);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|unique:' . Tag::class,
+        ]);
+        Tag::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+        ]);
+        return to_route('tags.index')->with('message', 'Tambah Tag Berhasil');
+    }
+
+    public function edit($id)
+    {
+        // Show a form to edit an existing tag
+        $tag = Tag::findOrFail($id);
+        return Inertia::render('Tags/Edit',[
+            'tag' => $tag,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Validate and update an existing tag
+        $tag = Tag::findOrFail($id);
+        $request->validate([
+            'name' => 'required|string|unique:'.Tag::class,
+        ]);
+        $tag->update($request->only(['name']));
+        $tag->slug = Str::slug($request->name);
+        $tag->save();
+        return to_route('tags.index')->with('message', 'tag berhasil di edit');
+
+    }
+
+    public function destroy($id)
+    {
+        // Delete an existing tag
+        Tag::destroy($id);
+        return to_route('tags.index')->with('message', 'Tag berhasil dihapus');
+    }
 }
