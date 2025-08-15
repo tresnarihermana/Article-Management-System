0e66e131e7d250d5e6fcff685ed7f0377a4768d9
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-15 21:57:32 +0700

    feat: (PageNav) update article links and improve layout

--- resources/js/components/PageNav.vue
+++ resources/js/components/PageNav.vue
@@ -20,9 +20,11 @@
         </button>
       </div>
       <PopoverGroup class="hidden lg:flex lg:gap-x-12">
-        <a href="#" class="text-sm/6 font-semibold text-gray-900 dark:text-gray-200 hover:text-green-600 dark:hover:text-green-500 ">Articles</a>
+        <a :href="route('articles.list')"
+          class="text-sm/6 font-semibold text-gray-900 dark:text-gray-200 hover:text-green-600 dark:hover:text-green-500 ">Articles</a>
         <Popover class="relative">
-          <PopoverButton class="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900 dark:text-gray-200  hover:text-green-600 dark:hover:text-green-500">
+          <PopoverButton
+            class="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900 dark:text-gray-200  hover:text-green-600 dark:hover:text-green-500">
             Categories
             <ChevronDownIcon class="size-5 flex-none text-gray-400" aria-hidden="true" />
           </PopoverButton>
@@ -31,11 +33,11 @@
             enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150"
             leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
             <PopoverPanel
-              class="absolute left-30 top-7 z-10 mt-3 w-screen max-w-7xl -translate-x-1/2 overflow-hidden rounded-xl dark:backdrop-blur-md bg-white shadow-lg ring-1 ring-gray-900/5 dark:bg-black/5">
+              class="absolute left-30 top-7 z-10 mt-3 w-screen max-w-7xl -translate-x-1/2 overflow-hidden rounded-xl dark:backdrop-blur-md bg-white shadow-lg ring-1 ring-gray-900/5 dark:bg-black/50">
 
-  
+
               <div class="p-8 grid grid-cols-3 gap-8">
-               
+
                 <div v-for="item in products" :key="item.name" class="flex gap-x-4">
                   <div
                     class="flex size-12 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white dark:bg-gray-800/50 dark:group-hover:bg-gray-800">
@@ -43,7 +45,8 @@
                       aria-hidden="true" />
                   </div>
                   <div>
-                    <a :href="item.href" class="block font-semibold text-gray-900 hover:text-green-600 dark:text-gray-300 dark:hover:text-green-500">
+                    <a :href="item.href"
+                      class="block font-semibold text-gray-900 hover:text-green-600 dark:text-gray-300 dark:hover:text-green-500">
                       {{ item.name }}
                     </a>
                     <p class="mt-1 text-gray-600 text-sm dark:text-gray-500">{{ item.description }}</p>
@@ -51,11 +54,12 @@
                 </div>
               </div>
 
-          
+
               <div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50 dark:bg-black/60">
                 <a v-for="item in callsToAction" :key="item.name" :href="item.href"
                   class="flex items-center justify-center gap-x-2.5 p-4 text-sm font-semibold text-gray-900 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-black/70 dark:hover:text-green-500">
-                  <component :is="item.icon" class="size-5 flex-none text-gray-400 dark:text-green-600 " aria-hidden="true" />
+                  <component :is="item.icon" class="size-5 flex-none text-gray-400 dark:text-green-600 "
+                    aria-hidden="true" />
                   {{ item.name }}
                 </a>
               </div>
@@ -64,30 +68,40 @@
           </transition>
         </Popover>
 
-        <a href="#" class="text-sm/6 font-semibold text-gray-900 dark:text-gray-200 hover:text-green-600 dark:hover:text-green-500">Marketplace</a>
-        <a href="#" class="text-sm/6 font-semibold text-gray-900 dark:text-gray-200 hover:text-green-600 dark:hover:text-green-500">About us</a>
+        <a href="#"
+          class="text-sm/6 font-semibold text-gray-900 dark:text-gray-200 hover:text-green-600 dark:hover:text-green-500">Marketplace</a>
+        <a href="#"
+          class="text-sm/6 font-semibold text-gray-900 dark:text-gray-200 hover:text-green-600 dark:hover:text-green-500">About
+          us</a>
       </PopoverGroup>
       <div class="hidden lg:flex lg:flex-1 lg:justify-end items-center gap-4">
-        <a v-if="!$page.props.auth.user" :href="route('login')" class="text-sm/6 font-semibold text-gray-900 dark:text-gray-300">
+        <SearchBar v-if="!$page.props.auth.user" />
+        <a v-if="!$page.props.auth.user" :href="route('login')"
+          class="text-sm/6 font-semibold text-gray-900 dark:text-gray-300">
           Log in <span aria-hidden="true">&rarr;</span>
         </a>
         <Popover v-if="$page.props.auth.user" class="relative hidden lg:flex lg:flex-1 lg:justify-end">
+          <SearchBar />
           <PopoverButton class="flex items-center gap-x-2 text-sm/6 font-semibold text-gray-900 dark:text-gray-300">
-            <img :src="$page.props.auth.user.avatar || 'https://ui-avatars.com/api/?name=' + $page.props.auth.user.name"
+            <img
+              :src="$page.props.auth.user.avatar ? '/storage/' + $page.props.auth.user.avatar : 'https://ui-avatars.com/api/?name=' + $page.props.auth.user.username"
               alt="Profile Photo" class="h-8 w-8 rounded-full object-cover" />
-            {{ $page.props.auth.user.name }}
+            {{ $page.props.auth.user.username }}
             <ChevronDownIcon class="size-5 flex-none text-gray-400" aria-hidden="true" />
           </PopoverButton>
 
           <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1"
             enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150"
             leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
-            <PopoverPanel class="absolute right-0 z-20 mt-9 w-48 rounded-2xl bg-white shadow-lg ring-1 ring-gray-900/5 dark:bg-black/60 dark:backdrop-blur-md">
+            <PopoverPanel
+              class="absolute right-0 z-20 mt-12 w-48 rounded-2xl bg-white shadow-lg ring-1 ring-gray-900/5 dark:bg-black/60 dark:backdrop-blur-md">
               <div class="py-2">
-                <a :href="route('profile.edit')" class="block px-4 py-2 text-sm/6 text-gray-700 hover:bg-gray-50 dark:text-gray-300 dark:hover:bg-green-700">
+                <a :href="route('profile.edit')"
+                  class="block px-4 py-2 text-sm/6 text-gray-700 hover:bg-gray-50 dark:text-gray-300 dark:hover:bg-green-700">
                   Edit Profile
                 </a>
-                <a :href="route('dashboard')" class="block px-4 py-2 text-sm/6 text-gray-700 hover:bg-gray-50 dark:text-gray-300 dark:hover:bg-green-700">
+                <a :href="route('dashboard')"
+                  class="block px-4 py-2 text-sm/6 text-gray-700 hover:bg-gray-50 dark:text-gray-300 dark:hover:bg-green-700">
                   Dashboard
                 </a>
                 <button :href="route('logout')" @click="handleLogout" type="submit"
@@ -109,9 +123,9 @@
         <div class="flex items-center justify-between">
           <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">Article Management System</span>
-          <div class="text-4xl font-bold dark:text-gray-200">
-            AMS<span class="text-green-700 dark:text-green-500">.</span>
-          </div>
+            <div class="text-4xl font-bold dark:text-gray-200">
+              AMS<span class="text-green-700 dark:text-green-500">.</span>
+            </div>
           </a>
           <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
             <span class="sr-only">Close menu</span>
@@ -121,6 +135,9 @@
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
+              <MobileSearchbar />
+              <a href="#"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-green-700 ">Articles</a>
               <Disclosure as="div" class="-mx-3" v-slot="{ open }">
                 <DisclosureButton
                   class="flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-green-700">
@@ -130,12 +147,11 @@
                 <DisclosurePanel class="mt-2 space-y-2">
                   <DisclosureButton v-for="item in [...products, ...callsToAction]" :key="item.name" as="a"
                     :href="item.href"
-                    class="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-green-700">{{
+                    class="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-green-700">
+                    {{
                       item.name }}</DisclosureButton>
                 </DisclosurePanel>
               </Disclosure>
-              <a href="#"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-green-700 ">Features</a>
               <a href="#"
                 class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-green-700">Marketplace</a>
               <a href="#"
@@ -151,10 +167,12 @@
                     <DisclosureButton
                       class="flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:hover:bg-green-700">
                       <img
-                        :src="$page.props.auth.user.avatar || 'https://ui-avatars.com/api/?name=' + $page.props.auth.user.name"
+                        :src="$page.props.auth.user.avatar ? '/storage/' + $page.props.auth.user.avatar : 'https://ui-avatars.com/api/?name=' + $page.props.auth.user.username"
                         alt="Profile Photo" class="h-8 w-8 rounded-full object-cover" />
-                      <span class="text-sm font-semibold text-gray-900 dark:text-gray-200 px-2"> {{ $page.props.auth.user.name }}</span>
-                      <ChevronDownIcon :class="[open ? 'rotate-180' : '', 'size-5 flex-none', 'dark:text-gray-200']" aria-hidden="true" />
+                      <span class="text-sm font-semibold text-gray-900 dark:text-gray-200 px-2"> {{
+                        $page.props.auth.user.username }}</span>
+                      <ChevronDownIcon :class="[open ? 'rotate-180' : '', 'size-5 flex-none', 'dark:text-gray-200']"
+                        aria-hidden="true" />
                     </DisclosureButton>
                     <DisclosurePanel class="mt-2 space-y-2">
                       <DisclosureButton as="a" :href="route('profile.edit')"
@@ -164,7 +182,7 @@
                         class="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-green-700">
                         Dashboard</DisclosureButton>
                       <DisclosureButton as="button" :href="route('logout')" @click="handleLogout"
-                        class="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-red-900 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-green-700">
+                        class="block w-full text-left rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-red-900 hover:bg-gray-50 dark:text-gray-200 dark:hover:bg-green-700">
                         Logout</DisclosureButton>
                     </DisclosurePanel>
                   </Disclosure>
@@ -199,13 +217,15 @@ import {
 } from '@heroicons/vue/24/outline'
 import { ChatBubbleLeftRightIcon, ChevronDownIcon, ComputerDesktopIcon, ListBulletIcon, NewspaperIcon, PencilIcon, RocketLaunchIcon, SparklesIcon } from '@heroicons/vue/20/solid'
 import ThemeMode from './ThemeMode.vue'
+import SearchBar from '@/components/SearchBar.vue';
+import MobileSearchbar from './MobileSearchbar.vue'
 
 const products = [
-  { name: 'Technology', description: 'Dapatkan informasi terbaru perkembangan teknologi', href: '#', icon: ComputerDesktopIcon },
-  { name: 'News', description: 'Kumpulan berita terkini paling faktual', href: '#', icon: NewspaperIcon },
-  { name: 'Life Style', description: 'Tips & tricks buat kamu yang mau improvisasi diri', href: '#', icon: SparklesIcon },
-  { name: 'Hobby', description: 'Dari diving di lautan hingga sky diving dari langit semua ada disini', href: '#', icon: RocketLaunchIcon },
-  { name: 'Review', description: 'Lihat pengalaman terseru mengenai game, software dan film!', href: '#', icon: ChatBubbleLeftRightIcon },
+  { name: 'Technology', description: 'Dapatkan informasi terbaru perkembangan teknologi', href: route('category.show', 'teknologi'), icon: ComputerDesktopIcon },
+  { name: 'News', description: 'Kumpulan berita terkini paling faktual', href: route('category.show', 'lifestyle'), icon: NewspaperIcon },
+  { name: 'Life Style', description: 'Tips & tricks buat kamu yang mau improvisasi diri', href: route('category.show', 'lifestyle'), icon: SparklesIcon },
+  { name: 'Hobby', description: 'Dari diving di lautan hingga sky diving dari langit semua ada disini', href: route('category.show', 'hobi'), icon: RocketLaunchIcon },
+  { name: 'Review', description: 'Lihat pengalaman terseru mengenai game, software dan film!', href: route('category.show', 'review'), icon: ChatBubbleLeftRightIcon },
   { name: 'See More', description: 'Lihat lebih banyak Kategori seru lainnya!', href: '#', icon: SquaresPlusIcon },
 ]
 const callsToAction = [
