c2e4296414cb77cc9ca828c3bdf825d94b0e418c
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-15 22:02:06 +0700

    feat: (Category) create Category page with CategorizedArticle component

---
+++ resources/js/pages/Main/Category.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import CategorizedArticle from '@/components/CategorizedArticle.vue';
+import PageLayout from '@/layouts/PageLayout.vue';
+import { Head, usePage } from '@inertiajs/vue3';
+const page = usePage()
+const {category} = page.props;
+</script>
+<template>
+
+    <PageLayout>
+        <CategorizedArticle :category="category"/>
+    </PageLayout>
+</template>
