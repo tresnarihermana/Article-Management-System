70a4a93f9af3187608c5a8c3527b79e668d17556
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-15 21:59:28 +0700

    feat: (MainPageLayout) add Toggle Theme button

--- resources/js/layouts/Main/PageSimpleLayout.vue
+++ resources/js/layouts/Main/PageSimpleLayout.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 import { Head, Link, usePage } from '@inertiajs/vue3';
 import PageNav from '@/components/PageNav.vue';
+import ThemeMode from '@/components/ThemeMode.vue';
 const { articles, tags, categories, users, title } =usePage().props;
 </script>
 
 <template>
-
-    <Head title="Welcome">
-        <link rel="preconnect" href="https://rsms.me/" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </Head>
     <header>
         <PageNav />
     </header>
      <slot />
+     <ThemeMode class="fixed bottom-10 right-5 lg:hidden bg-gray-300/40 hover:bg-gray-300 dark:hover:bg-gray-300/40 !rounded-full"></ThemeMode>
 </template>
