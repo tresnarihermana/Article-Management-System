2a2e6baa328dbe7c33875291c9bca132e87578f5
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-15 21:56:24 +0700

    chore: (HeroSection) i forgor

--- resources/js/components/MainHeroSection.vue
+++ resources/js/components/MainHeroSection.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 </script>
 <style>
 .dark .heroSection{
@@ -13,7 +12,7 @@
 
 </style>
 <template>
- <div class="flex flex-wrap py-15 heroSection">
+ <div class="flex flex-wrap fixed heroSection">
     <div class="w-full sm:w-8/12 mb-10 ">
       <div class="container mx-auto h-full sm:p-10 ">
         <header class="container px-4 lg:flex mt-10 items-center h-full lg:mt-0">
@@ -21,7 +20,7 @@
             <h1 class="text-4xl lg:text-6xl font-bold">Baca <span class="text-green-700">Article</span> terbaru hanya di AMS.</h1>
             <div class="w-20 h-2 bg-green-700 my-4"></div>
             <p class="text-xl mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae maiores neque eaque ea odit placeat, tenetur illum distinctio nulla voluptatum a corrupti beatae tempora aperiam quia id aliquam possimus aut.</p>
-            <button class="bg-green-500 text-white text-2xl font-medium px-4 py-2 rounded shadow">Learn More</button>
+            <a :href="route('login')" class="bg-green-700 cursor-pointer text-white text-2xl font-medium px-4 py-2 rounded-full shadow z-100">Getting Started</a>
           </div>
         </header>
       </div>
