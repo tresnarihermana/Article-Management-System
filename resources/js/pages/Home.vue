062e1b3d07d48cb3834a7561b0e2650958cd67c0
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-15 22:04:40 +0700

    feat: (Landing Page atau apalah) batasin user belum login cuman diliatin HeroSection

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
-import { Head, usePage } from '@inertiajs/vue3';
+import { Head, Link, usePage } from '@inertiajs/vue3';
+import PageNav from '@/components/PageNav.vue';
 import PageLayout from '@/layouts/PageLayout.vue';
 import MainHeroSection from '@/components/MainHeroSection.vue';
+import MainPinnedArticles from '@/components/MainPinnedArticles.vue';
+import LatestArticles from '@/components/LatestArticles.vue';
+import ThemeMode from '@/components/ThemeMode.vue';
 import ArticleList from '@/components/ArticleList.vue';
 import LatestArticleCarousel from '@/components/LatestArticleCarousel.vue';
 const { articles, tags, categories, users, title } = usePage().props;
 </script>
 
 <template>
-    <Head title="Welcome">
+    <Head title="Home">
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
     <PageLayout>
-        <MainHeroSection class="mx-auto relative max-md:pt-10" />
+        <MainHeroSection class="max-w-[2560px] mx-auto relative" />
+        <LatestArticleCarousel :articles="articles" class="pt-10" />
+        <ArticleList :categories="categories" class="max-w-[1200px] mx-auto relative" />
     </PageLayout>
 </template>
