fff86a70fd3af56bc3ac2c28edd8d12243ca2ecf
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-19 22:04:00 +0700

    feat: (comments and likes) implement comment and like features for articles

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function store(Request $request, $articleId)
+{
+    $request->validate([
+        'content' => 'required|string|max:1000',
+    ]);
+
+    Comment::create([
+        'article_id' => $articleId,
+        'user_id' => auth()->id(),
+        'content' => $request->content,
+    ]);
+
+    return redirect()->back();
+}
+
+}
