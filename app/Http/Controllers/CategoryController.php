26761b33d59c41b39f9a114db8533fdebf14a622
Tresnarihermana <Tresnarihermana@gmail.com>
2025-08-25 21:37:43 +0700

    feat! and feat: (DataTable) remove unused filter and pagination code and add bulkDestroy feature

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,35 +10,24 @@ use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
-public function index(Request $request)
+    public function index(Request $request)
     {
-        $perPage = $request->input('per_page', 10);
-        $search = $request->input('search');
         $categories = Category::with('articles')
-            ->when($search, function ($query, $search) {
-                $query->where(function ($q) use ($search) {
-                    $q->where('name', 'like', "%$search%")
-                        ->orWhere('slug', 'like', "%$search%");
-                });
-            })
             ->orderBy('id', 'desc')
-            ->paginate($perPage)
-            ->withQueryString()
-            ->through(function ($category) {
+            ->get()
+            ->map(function ($category) {
                 return [
                     'id' => $category->id,
                     'name' => $category->name,
                     'created_at' => $category->created_at->format('Y-m-d H:i:s'),
                     'updated_at' => $category->updated_at->format('Y-m-d H:i:s'),
                     'description' => $category->description,
-                    'articles' => $category->articles,
+                    'articles' => $category->articles->count(),
                     'slug' => $category->slug,
                 ];
             });
         return Inertia::render('Categories/Index', [
             'categories' => $categories,
-            'search' => $search,
-            'filters' => $request->only('input'),
 
         ]);
     }
@@ -46,16 +35,14 @@ public function index(Request $request)
     public function create()
     {
         // Show the form for creating a new resource.
-        return Inertia::render('Categories/Create',[
-            
-        ]);
+        return Inertia::render('Categories/Create', []);
     }
 
     public function store(Request $request)
     {
         // Store a newly created resource in storage.
         $request->validate([
-            'name' => 'required|string|unique:'.Category::class,
+            'name' => 'required|string|unique:' . Category::class,
             'description' => 'required|string|max:255'
         ]);
         $category = Category::create([
@@ -74,7 +61,7 @@ public function index(Request $request)
     public function edit($id)
     {
         $category = Category::findorFail($id);
-        return Inertia::render('Categories/Edit',[
+        return Inertia::render('Categories/Edit', [
             'category' => $category
         ]);
     }
@@ -82,7 +69,7 @@ public function index(Request $request)
     public function update(Request $request, $id)
     {
         $request->validate([
-            'name' => 'required|string|unique:'. Category::class,
+            'name' => 'required|string|unique:' . Category::class,
             'description' => 'required|string|max:255'
         ]);
         $category = Category::findOrFail($id);
@@ -90,7 +77,6 @@ public function index(Request $request)
         $category->slug = Str::slug($request->name);
         $category->save();
         return to_route('categories.index')->with('message', 'Category berhasil di Ubah');
-      
     }
 
     public function destroy($id)
@@ -98,5 +84,15 @@ public function index(Request $request)
         // Remove the specified resource from storage.
         Category::destroy($id);
         return to_route('categories.index')->with('message', 'category berhasil di hapus');
+    }
+    public function bulkDestroy(Request $request)
+    {
+        $ids = $request->input('ids', []);
+        // dd($ids);
+        if (!empty($ids)) {
+            Category::whereIn('id', $ids)->delete();
+        }
+
+        return redirect()->back(303)->with('success', 'Selected users deleted successfully');
     }
 }
