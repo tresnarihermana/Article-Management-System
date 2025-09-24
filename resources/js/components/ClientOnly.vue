4dedb17209a5a60eba00f80ccb031d262905e955
Tresnarihermana <Tresnarihermana@gmail.com>
2025-09-24 20:44:58 +0700

    ini lupa buat apa

---
+++ resources/js/components/ClientOnly.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+const isMounted = ref(false);
+onMounted(() => isMounted.value = true);
+</script>
+
+<template>
+  <slot v-if="isMounted" />
+</template>
